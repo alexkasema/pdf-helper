af2815794a56472b19359cade1dc0c17f03104c1
AlexKasema <lexxmulwa@gmail.com>
2024-09-30 17:19:25 +0300

    Created the endpoint /api/message for asking a question to a pdf file and streaming API responses in real time. Created the model Message for the database.

---
+++ src/app/api/message/route.ts
@@ -0,0 +1,28 @@
+import { db } from "@/db";
+import { SendMessageValidator } from "@/lib/validators/SendMessageValidator";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextRequest } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+  //! endpoint for asking a question to a pdf file
+  const body = await req.json();
+
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  const { id: userId } = user;
+
+  if (!userId) return new Response("Unauthorized", { status: 401 });
+
+  //! accessing the body content using Zod
+  const { fileId, message } = SendMessageValidator.parse(body);
+
+  const file = await db.file.findFirst({
+    where: {
+      id: fileId,
+      userId,
+    },
+  });
+
+  if (!file) return new Response("Not found", { status: 404 });
+};
