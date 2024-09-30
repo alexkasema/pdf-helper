36579876f98c05a1805cfa514b7b990c890367f0
AlexKasema <lexxmulwa@gmail.com>
2024-09-30 16:32:22 +0300

    Created a ChatContext component to act as a context wrapper so that we can pass data from ChatInput and Messages components.

---
+++ src/components/chat/ChatContext.tsx
@@ -0,0 +1,69 @@
+import { useToast } from "@/hooks/use-toast";
+import { useMutation } from "@tanstack/react-query";
+import { createContext, ReactNode, useState } from "react";
+
+type StreamResponse = {
+  addMessage: () => void;
+  message: string;
+  handleInputChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  isLoading: boolean;
+};
+
+export const ChatContext = createContext<StreamResponse>({
+  addMessage: () => {},
+  message: "",
+  handleInputChange: () => {},
+  isLoading: false,
+});
+
+interface Props {
+  fileId: string;
+  children: ReactNode;
+}
+
+//! what allows us to wrap our components
+//! responsible for all the logic in sending and receiving messages
+export const ChatContextProvider = ({ fileId, children }: Props) => {
+  const [message, setMessage] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const { toast } = useToast();
+
+  //! we are not using trpc because we want to stream back a response from the api to the client side/server side
+  const { mutate: sendMessage } = useMutation({
+    mutationFn: async ({ message }: { message: string }) => {
+      const response = await fetch("/api/message", {
+        method: "POST",
+        body: JSON.stringify({
+          fileId,
+          message,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to send message");
+      }
+
+      return response.body;
+    },
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(e.target.value);
+  };
+
+  const addMessage = () => sendMessage({ message });
+
+  return (
+    <ChatContext.Provider
+      value={{
+        addMessage,
+        message,
+        handleInputChange,
+        isLoading,
+      }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
